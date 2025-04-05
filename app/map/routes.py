7c4091c8df55f1ca5abebc371b4cf3bc9ff3f172
ulna <xianxingyuan520@outlook.com>
2025-04-05 22:03:57 +0800

    更新地图展示页面

--- app/map/routes.py
+++ app/map/routes.py
@@ -3,8 +3,18 @@ from . import map #导入蓝图
 
 
 @map.route('/')
-def map_view():
+def mapView():
     """显示地图页面"""
     return render_template('map.html')
 
 
+@map.route('/api/scenic_spots')
+def scenicSpots():
+    """获取景点数据"""
+    
+
+
+@map.route('/api/poi')
+def getPoi():
+    """获取兴趣点数据"""
+    
