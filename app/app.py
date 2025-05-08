649079744a46530b9e7499eea701b2822dc9d6cb
damie-sheep <zwy2300100918@163.com>
2025-05-09 01:06:14 +0800

    日记系统可能需要的文件，仅占位

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from app.map import map
 from app.login import login
 from app.api import api
 from app.spots import spots
+from app.diary import diary
 import os
 
 app = Flask(__name__)
@@ -15,6 +16,9 @@ app.register_blueprint(login,url_prefix="/login")
 app.register_blueprint(api,url_prefix="/api")
 
 app.register_blueprint(spots, url_prefix='/spots')
+
+# 旅游日记的蓝图注册
+# app.register_blueprint(diary,url_prefix='/diary')
 
 # 设置密钥，在生产环境应使用随机生成的强密钥
 app.secret_key = os.environ.get('SECRET_KEY', os.urandom(24))
